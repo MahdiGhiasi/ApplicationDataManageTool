6ad7de3267201feacc36cc87c81972eae871c0be
Mahdi <mghiasi75@gmail.com>
2016-08-09 21:26:14 +0430

    BoolToVisConverter

---
+++ App5/BoolToVis.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App5
+{
+    public class BoolToVisConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            try
+            {
+                if ((bool)value)
+                {
+                    return Visibility.Visible;
+                }
+                else
+                {
+                    return Visibility.Collapsed;
+                }
+            }
+            catch
+            {
+                return Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            try
+            {
+                if ((bool)value)
+                {
+                    return Visibility.Visible;
+                }
+            }
+            catch { }
+
+            return Visibility.Collapsed;
+
+        }
+    }
+}
