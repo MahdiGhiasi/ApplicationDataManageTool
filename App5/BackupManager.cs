ce3f725c9c488164ac0adabfda66b947e7879108
Mahdi <mghiasi75@gmail.com>
2016-08-12 01:47:07 +0430

    Backup now works completely

--- App5/BackupManager.cs
+++ App5/BackupManager.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip;
+using LightBuzz.Archiver;
+using SharpCompress.Archive.Zip;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +13,7 @@ namespace App5
 {
     public enum BackupState
     {
-        Initializing, CopyingFolders, CopyingFiles, Compressing, WritingMetadata
+        Initializing, CopyingFolders, CopyingFiles, Compressing, WritingMetadata, DeletingTemp, Finished
     }
 
 
@@ -34,6 +38,11 @@ namespace App5
 
     public class BackupManager
     {
+        private const double _CopyPercent = 0.45;
+        private const double _CompressPercent = 0.5;
+        private const double _DeletePercent = 0.04;
+        private const double _WriteMetadataPercent = 0.01;
+
         public delegate void BackupEventHandler(object sender, BackupEventArgs e);
         public event BackupEventHandler BackupProgress;
 
@@ -76,7 +85,7 @@ namespace App5
             string packagesBackupPath = System.IO.Path.Combine(backupPath, "Packages");
             await FileOperations.CreateDirectory(backupPath);
             await FileOperations.CreateDirectory(packagesBackupPath);
-            totalProgressFactor = (1.0 / apps.Count) / 2.0;
+            totalProgressFactor = (1.0 / apps.Count) * _CopyPercent;
             for (int i = 0; i < apps.Count; i++)
             {
                 completedCount = i;
@@ -91,6 +100,34 @@ namespace App5
 
                 copier.Copying -= Copier_Copying;
             }
+
+            OnBackupProgress(new BackupEventArgs(-1, _CopyPercent * 100.0, BackupState.Compressing, "Creating archive...", log));
+            StorageFolder packagesBackupFolder = await StorageFolder.GetFolderFromPathAsync(packagesBackupPath);
+            await CreateZip(packagesBackupFolder, System.IO.Path.Combine(backupPath, "data.zip"), System.IO.Compression.CompressionLevel.Optimal);
+            
+            OnBackupProgress(new BackupEventArgs(-1, (_CopyPercent + _CompressPercent) * 100.0, BackupState.DeletingTemp, "Deleting Temporary data...", log));
+            await packagesBackupFolder.DeleteAsync();
+
+            OnBackupProgress(new BackupEventArgs(-1, (_CopyPercent + _CompressPercent + _DeletePercent) * 100.0, BackupState.WritingMetadata, "Creating metadata...", log));
+
+            Backup currentBackup = new Backup(name);
+            foreach (var item in apps)
+            {
+                currentBackup.Apps.Add(new CompactAppData(item));
+            }
+
+            await WriteMetaData(currentBackup, System.IO.Path.Combine(backupPath, "metadata.json"));
+
+            OnBackupProgress(new BackupEventArgs(0, 100.0, BackupState.Finished, "Finalizing...", log));
+        }
+
+        private async Task WriteMetaData(Backup backup, string metadataFile)
+        {
+            string data = Newtonsoft.Json.JsonConvert.SerializeObject(backup, Newtonsoft.Json.Formatting.Indented);
+
+            StorageFile metadata = await (await StorageFolder.GetFolderFromPathAsync(System.IO.Path.GetDirectoryName(metadataFile))).CreateFileAsync(System.IO.Path.GetFileName(metadataFile));
+
+            await FileIO.WriteTextAsync(metadata, data);
         }
 
         private void Copier_Copying(object sender, FileOperations.CopyingEventArgs e)
@@ -114,6 +151,22 @@ namespace App5
                                                      status,
                                                      curLog));
             }
+        }
+        
+
+        public async Task CreateZip(StorageFolder backupFolder, string zipFileName, System.IO.Compression.CompressionLevel compressionLevel)
+        {
+            ArchiverPlus archiver = new ArchiverPlus();
+            archiver.CompressingProgress += Archiver_CompressingProgress;
+
+            await archiver.Compress(backupFolder, await (await StorageFolder.GetFolderFromPathAsync(System.IO.Path.GetDirectoryName(zipFileName))).CreateFileAsync(System.IO.Path.GetFileName(zipFileName)), compressionLevel);
+
+            archiver.CompressingProgress -= Archiver_CompressingProgress;
+        }
+
+        private void Archiver_CompressingProgress(object sender, CompressingEventArgs e)
+        {
+            OnBackupProgress(new BackupEventArgs(e.Percent * 100.0, (_CopyPercent + e.Percent * _CompressPercent) * 100.0, BackupState.Compressing, "Creating archive...", log));
         }
 
         public void Restore(Backup backup)
