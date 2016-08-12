2a5dc80f4e8b08bcfab9286f19c1851d21e75716
Mahdi <mghiasi75@gmail.com>
2016-08-12 16:38:51 +0430

    Subtitle and Phone info added to Metadata

--- App5/Backup.cs
+++ App5/Backup.cs
@@ -1,21 +1,49 @@
-﻿using System;
+﻿using Ailon.WP.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Networking.Connectivity;
+using Windows.Security.ExchangeActiveSyncProvisioning;
 
 namespace App5
 {
     public class Backup
     {
-        public Backup(string _name)
+        public Backup(string _name, string _subtitle)
         {
             Name = _name;
             Apps = new List<CompactAppData>();
+            Subtitle = _subtitle;
+        }
+
+        public void SetDeviceInfo()
+        {
+            var deviceInfo = new Windows.Security.ExchangeActiveSyncProvisioning.EasClientDeviceInformation();
+            DeviceName = deviceInfo.FriendlyName;
+
+            DeviceModel = PhoneNameResolver.Resolve(deviceInfo.SystemManufacturer, deviceInfo.SystemProductName).FullCanonicalName;
+
+            CreationDate = DateTime.Now;
         }
 
         public string Name { get; set; }
         public List<CompactAppData> Apps { get; set; }
+        public string Subtitle { get; set; }
+        public string DeviceModel { get; set; }
+        public string DeviceName { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        internal static string GenerateAppSubtitle(List<CompactAppData> apps)
+        {
+            return apps.Count == 1 ? apps[0].DisplayName : (apps.Count.ToString() + " apps");
+        }
+
+        internal static string GenerateAppSubtitle(List<AppData> apps)
+        {
+            return apps.Count == 1 ? apps[0].DisplayName : (apps.Count.ToString() + " apps");
+        }
     }
 
     public class CompactAppData
